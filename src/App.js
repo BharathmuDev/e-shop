045e832e823a4d3dcc8b7ed4bdde1c115cdde8d9
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-08-14 14:07:18 +0530

    add announcement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Announcement from "./pages/components/Announcement";
 import Navbar from "./pages/components/Navbar";
 
 function App() {
   return (
     <div>
+      <Announcement />
       <Navbar />
     </div>
   );
