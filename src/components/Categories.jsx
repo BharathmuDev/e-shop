b4846d7c174e7342880f4a0da6c75a4758b1a14b
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-08-15 12:55:17 +0530

    Add categories component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+import CategoryItem from './CategoryItem';
+import { categories } from '../data';
+
+const Container = styled.div`
+display: flex;
+padding: 20px;
+justify-content: space-between;
+`;
+
+const Categories = () => {
+  return (
+    <Container>
+            {categories.map(item=> (
+            <CategoryItem item={item}/>
+        ))}
+    </Container>
+  )
+}
+
+export default Categories
