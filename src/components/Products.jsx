f55cb4cd683b48538081dbe486a7390868d73481
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-08-16 15:44:20 +0530

    add animation styles to products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,11 @@ import { popularProducts } from '../data';
 import Product from './Product';
 
 const Container = styled.div`
-`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
 
 const Products = () => {
   return (
